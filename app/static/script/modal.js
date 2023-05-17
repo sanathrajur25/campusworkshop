f97f201d8ae16c6371372fc66416508c6bb3f2df
sanathrajur25 <sanathrajur@gmail.com>
2023-05-17 14:48:47 +0530

    changed

--- app/static/script/modal.js
+++ app/static/script/modal.js
@@ -92,6 +92,9 @@ $(document).ready(function () {
         if (state.text() === "Todo") {
             new_state = "In Progress"
         }
+        if(state.text()==="In Progress"){
+            new_state="Completed"
+        }
         
        
         console.log(new_state)
